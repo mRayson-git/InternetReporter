b47f582f9be71a094bed4774c6e55352c33f4914
Michael Rayson <mrayson5129@gmail.com>
2021-04-13 15:23:18 -0400

    Added timezone settings

--- data_collector/app.js
+++ data_collector/app.js
@@ -1,23 +1,53 @@
-const exec = require("child_process");
+const { exec } = require("child_process");
 const fs = require('fs');
+const dns = require('dns');
+const fetch = require('node-fetch');
 
 let currDate = new Date();
-let speedDir = './speed_data/'
-let connectionDir = './speed_data/'
-let iteration = 0;
+let speedDir = './speed_data/';
+let connectionDir = './connection_data/';
 
-// Runs forever
-while (true) {
+console.log(process.env.TZ);
+
+setInterval(async () => {
+    // Variables
     let today = new Date();
+    const datestring = currDate.getFullYear() + '-' + currDate.getMonth() + '-' + currDate.getDay();
+    console.log(datestring);
+    let speedFile = speedDir+datestring+'.txt';
+    let connectivityFile = connectionDir+datestring+'.txt';
+
     // If its a new date make new folders for data
-    if (today > currDate) {
+    if (today.toLocaleDateString() >= currDate.toLocaleDateString()) {
         currDate = today;
         iteration = 0;
-        fs.mkdirSync(speedDir+currDate.toLocaleDateString());
-        fs.mkdirSync(connectionDir+currDate.toLocaleDateString());
+        speedFile = speedDir+datestring+'.txt';
+        connectivityFile = connectionDir+datestring+'.txt';
     }
-    // If its the tenth iteration do full check
-    if (iteration % 10 == 0) {
-        exec('speedtest -j > ')
+
+    // Check connectivity
+    console.log('Checking connectivity...');
+    let connected = await fetch('https://google.com').then(ok => {
+        console.log('Done checking');
+        return true;
+    }).catch(err => {
+        // console.log(err);
+        return false;
+    });
+
+    // Add connectivity information to log file
+    if (connected) {
+        console.log(`Connected\n${today.toLocaleTimeString()}, true\n`);
+        fs.appendFileSync(connectivityFile, `${today.toLocaleTimeString()}, true\n`, err => {
+            if (err) throw err;
+        });
+    } else {
+        console.log('Not connected');
+        fs.appendFileSync(connectivityFile, `${today.toLocaleTimeString()}, false\n`, err => {
+            if (err) throw err;
+        });
     }
-}
+
+}, 5000);
+
+
